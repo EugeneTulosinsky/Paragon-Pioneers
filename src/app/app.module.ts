b180edca28ba3148db2a899c29d80c42f1095bc6
EugeneTulosinsky <etulosinsky@allinsight.de>
2022-12-01 16:30:12 +0100

    auto-completion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './angular-material/material.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductionsListComponent } from './productions-list/productions-list.component';
 import { ProductionComponent } from './production/production.component';
+import { AutoCompletionComponent } from "./auto-completion/auto-completion.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    AutoCompletionComponent,
     ProductionsListComponent,
-    ProductionComponent
+    ProductionComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  exports: [
+    AutoCompletionComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
