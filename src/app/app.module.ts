507aecda43dc6b490a0f9c8fa1e477821cda7499
Matti Elsing <m.elsing@abtis.de>
2022-12-02 14:03:17 +0100

    Add component and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductionsListComponent } from './productions-list/productions-list.component';
 import { ProductionComponent } from './production/production.component';
+import { IconSheetComponent } from './icon-sheet/icon-sheet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductionsListComponent,
-    ProductionComponent
+    ProductionComponent,
+    IconSheetComponent
   ],
   imports: [
     BrowserModule,
